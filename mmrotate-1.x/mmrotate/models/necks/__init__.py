f6cd44968f2a92cf94da5165ce21f8b80b9ce274
ssdle <162157220+ssdle@users.noreply.github.com>
2025-07-24 23:37:29 +0800

    Delete mmrotate-1.x/mmrotate/models/necks directory

--- mmrotate-1.x/mmrotate/models/necks/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .re_fpn import ReFPN
-from .fpnformer_retinanet import FPNdecoderformer_swin_double
-from .fpnformer_retinanet_ssdd import FPNformer_ssdd
-
-__all__ = ['ReFPN', 'FPNdecoderformer_swin_double', 'FPNformer_ssdd']
