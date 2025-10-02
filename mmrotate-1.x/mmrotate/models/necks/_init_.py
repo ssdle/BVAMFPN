8fab80ba8b2f9c26abea6083510cff2b5dc81ca4
ssdle <162157220+ssdle@users.noreply.github.com>
2025-10-02 13:35:27 +0800

    _init_.py

--- mmrotate-1.x/mmrotate/models/necks/_init_.py
+++ mmrotate-1.x/mmrotate/models/necks/_init_.py
@@ -3,4 +3,5 @@ from .re_fpn import ReFPN
 from .fpnformer_retinanet import FPNdecoderformer_swin_double
 from .fpnformer_retinanet_ssdd import FPNformer_ssdd
 from .BVAMFPN import BVAMFPN
-__all__ = ['ReFPN', 'FPNdecoderformer_swin_double', 'FPNformer_ssdd','BVAMFPN']
+from .BVAM_rsdd import BVAMFPN_rsdd
+__all__ = ['ReFPN', 'FPNdecoderformer_swin_double', 'FPNformer_ssdd','BVAMFPN','BVAMFPN_rsdd']
